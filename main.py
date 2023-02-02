fadecc9eff0953902d4297c856acb63d889aca6f
viseryon <alansliwinski7@gmail.com>
2023-02-02 23:04:16 +0100

    od#tagowanie

--- main.py
+++ main.py
@@ -12,21 +12,26 @@ def autoryzacja():
     access_token_secret = keys.access_token_secret
     api_secret = keys.api_secret
 
-    client, api = twitter.au(bearer_token, api_key, api_secret, access_token, access_token_secret)
+    client, api = twitter.au(
+        bearer_token, 
+        api_key,
+        api_secret, 
+        access_token, 
+        access_token_secret
+        )
+    
     print('auth completed')
 
     return client, api
-
 
 
 def main():
 
     client, api = autoryzacja()
 
-    # wig_main.main(client, api)
+    wig_main.main(client, api)
 
     makro_main.main(client, api)
-
 
 
 if __name__ == '__main__':
