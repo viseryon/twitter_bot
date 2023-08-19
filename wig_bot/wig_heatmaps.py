e1c36ffb6244815d8e6750314c75ac4028fa7370
Alan Śliwiński <113923247+viseryon@users.noreply.github.com>
2023-08-19 20:40:19 +0200

    fixed paths

--- wig_bot/wig_heatmaps.py
+++ wig_bot/wig_heatmaps.py
@@ -290,7 +290,7 @@ def wig_sectors_do_chart_1w_perf():
     today = dt.today()
     week_nr = today.isocalendar().week
 
-    df = pd.read_csv("wig_bot/sectors.csv")
+    df = pd.read_csv("sectors.csv")
     df.Ticker = df.Ticker + ".WA"
 
     prices = yq.Ticker(df.Ticker).history(period="1mo")[["adjclose"]].reset_index()
@@ -863,7 +863,7 @@ def wig_do_chart_1w_perf():
     today = dt.today()
     week_nr = today.isocalendar().week
 
-    df = pd.read_csv("wig_bot/wig.csv")
+    df = pd.read_csv("wig.csv")
 
     df.Ticker = df.Ticker + ".WA"
 
