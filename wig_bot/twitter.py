88ff33be20044ddc435a5a85a7a7a184251c3841
Alan Śliwiński <113923247+viseryon@users.noreply.github.com>
2023-08-27 12:59:59 +0200

    run black formatter

--- wig_bot/twitter.py
+++ wig_bot/twitter.py
@@ -1,31 +1,33 @@
 import tweepy
 
+
 def au(bearer_token, api_key, api_secret, access_token, access_token_secret):
-    client = tweepy.Client(bearer_token, api_key,
-                        api_secret, access_token, access_token_secret)
+    client = tweepy.Client(
+        bearer_token, api_key, api_secret, access_token, access_token_secret
+    )
 
     auth = tweepy.OAuth1UserHandler(
-        api_key, api_secret, access_token, access_token_secret)
+        api_key, api_secret, access_token, access_token_secret
+    )
     api = tweepy.API(auth)
 
     return client, api
 
-def tweet_things(client, api, text='test', obrazki=None):
 
+def tweet_things(client, api, text="test", obrazki=None):
     if obrazki:
         lst = []
         for obrazek in obrazki:
             media = api.media_upload(filename=obrazek)
             lst.append(media.media_id_string)
-        
+
         client.create_tweet(text=text, media_ids=lst)
 
     else:
         client.create_tweet(text=text)
 
+    print("tweeted successfully")
 
-    print('tweeted successfully')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
