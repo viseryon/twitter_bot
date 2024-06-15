2b8bde325587e932d4e97ae1f473e8cbb0e81f4f
Alan Śliwiński <113923247+viseryon@users.noreply.github.com>
2024-06-15 15:43:12 +0200

    changed main file

--- main.py
+++ main.py
@@ -1,32 +1,156 @@
+import os
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import yahooquery as yq
+from matplotlib import pyplot as plt
+from tweepy import API, Client, OAuth1UserHandler
+
 import keys
-import twitter
-import wig_bot.main as wig_main
+
+plt.style.use("dark_background")
+
+os.chdir(Path(__file__).parent)
 
 
-def autoryzacja():
-    bearer_token = keys.bearer_token
-    api_key = keys.api_key
-    access_token = keys.access_token
-    access_token_secret = keys.access_token_secret
-    api_secret = keys.api_secret
+class TwitterBot:
+    def __init__(self) -> None:
+        # client, api = self.auth()
+        # self.client: Client = client
+        # self.api: API = api
 
-    client, api = twitter.au(
-        bearer_token, api_key, api_secret, access_token, access_token_secret
-    )
+        wig_components = self._get_wig_components()
+        self.wig_components: pd.DataFrame = wig_components
+        self.tickers: list = wig_components.ticker.to_list()
 
-    print("auth completed")
+        # self.prices = self._get_data()
 
-    return client, api
+    def _au(
+        self, bearer_token, api_key, api_secret, access_token, access_token_secret
+    ) -> tuple[Client, API]:
+        client = Client(
+            bearer_token, api_key, api_secret, access_token, access_token_secret
+        )
 
+        auth = OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+        api = API(auth, retry_count=5, retry_delay=5, retry_errors=set([503]))
+        # https://stackoverflow.com/questions/48117126/when-using-tweepy-cursor-what-is-the-best-practice-for-catching-over-capacity-e
+        return client, api
 
-def main():
-    client, api = autoryzacja()
+    def auth(self) -> tuple[Client, API]:
+        bearer_token = keys.BEARER_TOKEN
+        api_key = keys.API_KEY
+        access_token = keys.ACCESS_TOKEN
+        access_token_secret = keys.ACCESS_TOKEN_SECRET
+        api_secret = keys.API_SECRET
 
-    wig_main.main(client, api)
+        client, api = self._au(
+            bearer_token, api_key, api_secret, access_token, access_token_secret
+        )
 
-    makro_main.main(client, api)
+        return client, api
+
+    def _make_tweet(self, text: str, pictures=None):
+        if pictures:
+            lst = []
+            for picture in pictures:
+                media = self.api.media_upload(filename=picture)
+                lst.append(media.media_id_string)
+
+            self.client.create_tweet(text=text, media_ids=lst)
+
+        else:
+            self.client.create_tweet(text=text)
+
+    def _get_data(self) -> pd.DataFrame:
+
+        tickers = yq.Ticker(self.tickers)
+        self._yq_tickers = tickers
+
+        prices = tickers.history()[["adjclose"]]
+        prices = prices.reset_index().pivot(
+            index="date", columns="symbol", values="adjclose"
+        )
+
+        return prices
+
+    @staticmethod
+    def get_symbol(query, preferred_exchange="WSE"):
+        try:
+            data = yq.search(query)
+        except ValueError:  # Will catch JSONDecodeError
+            print(query)
+        else:
+            quotes = data["quotes"]
+            if len(quotes) == 0:
+                return "No Symbol Found"
+
+            symbol = quotes[0]["symbol"]
+            for quote in quotes:
+                if quote["exchange"] == preferred_exchange:
+                    symbol = quote["symbol"]
+                    break
+            return symbol
+
+    def _get_wig_components(self) -> pd.DataFrame:
+
+        # get data from source
+        updated_components = pd.read_html(
+            "https://gpwbenchmark.pl/ajaxindex.php?action=GPWIndexes&start=ajaxPortfolio&format=html&lang=EN&isin=PL9999999995&cmng_id=1011&time=1718378430237"
+        )[0]
+
+        updated_components = updated_components.iloc[:, :3]
+        updated_components.columns = ["company", "ISIN", "shares_num"]
+
+        saved_components = pd.read_csv("wig_comps.csv")
+
+        # add tickers to the source data
+        full_components = pd.merge(
+            saved_components, updated_components, how="right", on=["company", "ISIN"]
+        )
+
+        # check for empty data
+        empty_data = full_components[full_components.isnull().any(axis=1)]
+        print("empty data")
+        print(empty_data)
+        if empty_data.empty:
+            return full_components
+        else:  # get new ticker from Yahoo Finance
+            for indx, (
+                company,
+                isin,
+                ticker,
+                sector,
+                industry,
+                shares_num,
+            ) in empty_data.iterrows():
+                print(f"Company {company} had missing data.")
+
+                if ticker is np.NaN:
+                    # add missing ticker
+                    new_symbol = self.get_symbol(company)
+                    full_components.loc[indx, "ticker"] = new_symbol
+
+                if sector is np.NaN or industry is np.NaN:
+                    # add missing sector and industry values
+                    asset_profile = yq.Ticker(new_symbol).asset_profile[new_symbol]
+                    full_components.loc[indx, "sector"] = asset_profile["sector"]
+                    full_components.loc[indx, "industry"] = asset_profile["industry"]
+
+            # save new csv with full WIG
+            full_components.drop(columns="shares_num").to_csv(
+                "wig_comps.csv", index=False
+            )
+
+        return full_components
+
+    def run(self):
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
-    main()
-    # tycia zmiana
+    bot = TwitterBot()
+    bot.run()
