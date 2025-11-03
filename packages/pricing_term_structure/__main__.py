884c0e0ecb3c9f300d241fb5846d4281cafb6abe
viseryon <viseryon@users.noreply.github.com>
2025-11-03 20:06:56 +0100

    add --force flag to force the script to post

--- packages/pricing_term_structure/__main__.py
+++ packages/pricing_term_structure/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 
 from bot import TermStructureBot
@@ -5,8 +6,28 @@ from mylogging import setup
 
 logger = setup(__name__, __file__)
 
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Example script with --force flag")
+
+    parser.add_argument(
+        "--force",
+        "-f",
+        action="store_true",
+        default=False,
+        help="Force the action (skip confirmations)",
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    if datetime.today().day == 1:
+    args = parse_args()
+    if args.force:
+        logger.info("forced to post, running pricing term structure project")
+        bot = TermStructureBot()
+        bot.run()
+    elif datetime.today().day == 1:
         logger.info("it's first of the month, running pricing term structure project")
         bot = TermStructureBot()
         bot.run()
